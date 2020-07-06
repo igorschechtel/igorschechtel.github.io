30cfbb53a36cafd261367caf1fdf4132ec838822
Igor Schechtel <igorpschechtel@gmail.com>
2020-07-05 23:49:15 -0300

    deploy

---
+++ precache-manifest.6384a9f624d17dbfcfb7e97ac6eebd9d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b837aa8e370678db1c73207c2a5174",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fff081ea669e0446561d",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9d7408f67a0cf2ebaaa8",
+    "url": "/static/css/main.c336db3f.chunk.css"
+  },
+  {
+    "revision": "fff081ea669e0446561d",
+    "url": "/static/js/2.05d12f71.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.05d12f71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d7408f67a0cf2ebaaa8",
+    "url": "/static/js/main.f96a62dc.chunk.js"
+  },
+  {
+    "revision": "174b6f04b5fa9ca249fe",
+    "url": "/static/js/runtime-main.d1873c08.js"
+  },
+  {
+    "revision": "452f9e9473d9d488c9d1c05b023e29d3",
+    "url": "/static/media/background.452f9e94.jpg"
+  },
+  {
+    "revision": "32a19b58f78477316391ef3ad4d61860",
+    "url": "/static/media/logo.32a19b58.png"
+  }
+]);
